139c1a99fffb016844ce30f60eae6afd5a47a183
Tien Lam <tienlamnhatt@gmail.com>
2025-09-04 21:30:19 +0700

    intial commit

---
+++ backend/src/main/java/com/lnt/chatmee/ChatmeeApplication.java
@@ -0,0 +1,14 @@
+package com.lnt.chatmee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class ChatmeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatmeeApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/lnt/chatmee/ChatmeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lnt.chatmee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatmeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
