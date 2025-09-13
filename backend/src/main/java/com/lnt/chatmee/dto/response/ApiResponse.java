ffdb7dea0ceaf2c85db19ba188640e3532d089ba
Tien Lam <tienlamnhatt@gmail.com>
2025-09-13 23:48:47 +0700

    feat: add new response type for response without data

--- backend/src/main/java/com/lnt/chatmee/dto/response/ApiResponse.java
+++ backend/src/main/java/com/lnt/chatmee/dto/response/ApiResponse.java
@@ -21,6 +21,13 @@ public class ApiResponse<T> {
                 .build();
     }
 
+    public static <T> ApiResponse<T> success(String message) {
+        return ApiResponse.<T>builder()
+            .success(true)
+            .message(message)
+            .build();
+    }
+
     public static <T> ApiResponse<T> success(String message, T data) {
         return ApiResponse.<T>builder()
                 .success(true)
