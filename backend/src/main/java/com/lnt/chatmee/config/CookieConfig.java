2020c57cc5f32829b019321eb17a4167ea92f008
Tien Lam <tienlamnhatt@gmail.com>
2025-10-21 21:44:15 +0700

    Configure session cookies for cross-origin requests (SameSite=None, Secure)

---
+++ backend/src/main/java/com/lnt/chatmee/config/CookieConfig.java
@@ -0,0 +1,14 @@
+package com.lnt.chatmee.config;
+
+import org.springframework.boot.web.servlet.server.CookieSameSiteSupplier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class CookieConfig {
+
+    @Bean
+    public CookieSameSiteSupplier applicationCookieSameSiteSupplier() {
+        return CookieSameSiteSupplier.ofNone().whenHasName("JSESSIONID");
+    }
+}
