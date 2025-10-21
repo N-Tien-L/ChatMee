bbc4415368794efe48138a97549be68cc895a5fa
Tien Lam <tienlamnhatt@gmail.com>
2025-10-21 21:24:58 +0700

    Fix CORS to use client URL from environment variable

--- backend/src/main/java/com/lnt/chatmee/config/CorsConfig.java
+++ backend/src/main/java/com/lnt/chatmee/config/CorsConfig.java
@@ -2,6 +2,7 @@ package com.lnt.chatmee.config;
 
 import java.util.Arrays;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
@@ -11,11 +12,14 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 @Configuration
 public class CorsConfig {
 
+    @Value("${app.client.url}")
+    private String clientUrl;
+
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+        configuration.setAllowedOrigins(Arrays.asList(clientUrl));
 
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
 
